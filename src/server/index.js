94aa67300f501b186903466478051e460c7e629e
Tsuyoshi Akiyama <ka090824@gmail.com>
2018-11-04 13:21:01 +0900

    Add webpack middleware in server-side

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,4 @@
+/* eslint global-require: 0 */
 import express from 'express';
 import path from 'path';
 import logger from 'morgan';
@@ -5,14 +6,19 @@ import bodyParser from 'body-parser';
 
 const app = express();
 const port = process.env.PORT || 3000;
-const publicPath = path.resolve(__dirname, '..', '..', 'public');
+const publicPath = path.resolve(__dirname, '../../', 'public');
+const isProd = process.env.NODE_ENV === 'production';
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,POST');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type',
+  );
   res.set({
-    Connection: 'close', 'Cache-Control': 'no-cache',
+    Connection: 'close',
+    'Cache-Control': 'no-cache',
   });
 
   next();
@@ -25,8 +31,33 @@ app.get('/api', (req, res) => {
   res.send('Hello from Express!');
 });
 
-// It serves any static files
-app.use(express.static(publicPath));
-app.set('port', port);
+// Webpack Requirements
+if (!isProd) {
+  const webpack = require('webpack');
+  const config = require('../../webpack.config');
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  const compiler = webpack(config);
 
+  app.use(
+    webpackDevMiddleware(compiler, {
+      watchOptions: {
+        ignored: /node_modules/,
+      },
+      noInfo: true,
+      publicPath: config.output.path,
+      port,
+    }),
+  );
+  app.use(
+    webpackHotMiddleware(compiler, {
+      log: false,
+      heartbeat: 2000,
+    }),
+  );
+} else {
+  // It serves any static files in production
+  app.use(express.static(publicPath));
+  app.set('port', port);
+}
 export default app;
